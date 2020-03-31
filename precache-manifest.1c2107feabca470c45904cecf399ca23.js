b896213c6e453eca79b6412cd7a90ccfb5aae73d
Dan <dsack7@gmail.com>
2020-03-31 14:48:16 +0200

    Updates

---
+++ precache-manifest.1c2107feabca470c45904cecf399ca23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a04b60ae8987afb88140f5b370d593f",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "ecb33dc3608fbcdc8424",
+    "url": "/pomodoro-clock/static/css/main.4698341e.chunk.css"
+  },
+  {
+    "revision": "887a5e4e60fedb995340",
+    "url": "/pomodoro-clock/static/js/2.e1f290b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.e1f290b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb33dc3608fbcdc8424",
+    "url": "/pomodoro-clock/static/js/main.60594cd5.chunk.js"
+  },
+  {
+    "revision": "743f6ad71378fe6a80d1",
+    "url": "/pomodoro-clock/static/js/runtime-main.fda66761.js"
+  }
+]);
